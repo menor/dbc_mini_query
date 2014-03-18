90624a84f0155c40e34631df51b7030246f96752
& <pair+menor.mknudsen01@devbootcamp.com>
2014-03-18 10:47:46 -0700

    Add basic Dom methods, just hide and show.

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -7,11 +7,35 @@ var SweetSelector = {
     var symbol = selector[0];
     var element = selector.slice(1);
     if(symbol == '#'){
-      return document.getElementById(element)
+      return [document.getElementById(element)]
     } else if (symbol == '.'){
       return document.getElementsByClassName(element)
     } else {
       return document.getElementsByTagName(selector)
     }
+  }
+}
+
+var DOM = {
+  show: function(selector){
+    elements = SweetSelector.select(selector)
+    _.each(elements, function(element){
+      element.style.display="block";
+    })
+  },
+
+  hide: function(selector){
+    elements = SweetSelector.select(selector)
+    _.each(elements, function(element){
+      element.style.display="none";
+    })
+  },
+
+  addclass: function(selector){
+
+  },
+
+  removeClass: function(selector){
+
   }
 }
