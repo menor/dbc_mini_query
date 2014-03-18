401c70c0bb357030f6fae3d1cd8f7bd65b2efcd8
& <pair+menor.mknudsen01@devbootcamp.com>
2014-03-18 12:11:49 -0700

    Wrap everything under miniQuery umbrella.

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,89 +1,88 @@
-/*!
- * minQuery
- */
+var miniQuery = {
 
-var SweetSelector = {
-  select: function(selector){
-    var symbol = selector[0];
-    var element = selector.slice(1);
-    if(symbol == '#'){
-      return [document.getElementById(element)]
-    } else if (symbol == '.'){
-      return document.getElementsByClassName(element)
-    } else {
-      return document.getElementsByTagName(selector)
+  SweetSelector: {
+    select: function(selector){
+      var symbol = selector[0];
+      var element = selector.slice(1);
+      if(symbol == '#'){
+        return [document.getElementById(element)]
+      } else if (symbol == '.'){
+        return document.getElementsByClassName(element)
+      } else {
+        return document.getElementsByTagName(selector)
+      }
     }
-  }
-}
+  },
 
-var DOM = (function(sweetSelector){
-  return {
-    show: function(selector){
-      elements = sweetSelector.select(selector)
-      _.each(elements, function(element){
-        element.style.display="block";
-      })
-    },
+  DOM: (function(sweetSelector){
+    return {
+      show: function(selector){
+        elements = sweetSelector.select(selector)
+        _.each(elements, function(element){
+          element.style.display="block";
+        })
+      },
 
-    hide: function(selector){
-      elements = sweetSelector.select(selector)
-      _.each(elements, function(element){
-        element.style.display="none";
-      })
-    },
+      hide: function(selector){
+        elements = sweetSelector.select(selector)
+        _.each(elements, function(element){
+          element.style.display="none";
+        })
+      },
 
-    addClass: function(selector, className){
-      elements = sweetSelector.select(selector)
-      _.each(elements, function(element){
-        element.classList.add(className);
-      })
-    },
+      addClass: function(selector, className){
+        elements = sweetSelector.select(selector)
+        _.each(elements, function(element){
+          element.classList.add(className);
+        })
+      },
 
-    removeClass: function(selector, className ){
-      elements = sweetSelector.select(selector)
-      _.each(elements, function(element){
-        element.classList.remove(className);
-      })
+      removeClass: function(selector, className ){
+        elements = sweetSelector.select(selector)
+        _.each(elements, function(element){
+          element.classList.remove(className);
+        })
+      }
     }
-  }
-}(SweetSelector));
+  }(this.SweetSelector)),
 
-var EventDispatcher = (function(sweetSelector){
-  return {
-    on: function( selector, event, callback ) {
-      elements = sweetSelector.select( selector )
-      _.each( elements, function( element ) {
-        element.addEventListener( event, callback )
-      })
-    },
+  EventDispatcher: (function(sweetSelector){
+    return {
+      on: function( selector, event, callback ) {
+        elements = sweetSelector.select( selector )
+        _.each( elements, function( element ) {
+          element.addEventListener( event, callback )
+        })
+      },
 
-    trigger: function( selector, event ) {
-      our_event = new CustomEvent( event )
-      elements = sweetSelector.select( selector )
-      _.each( elements, function( element ) {
-        !element.dispatchEvent( our_event )
-      })
+      trigger: function( selector, event ) {
+        our_event = new CustomEvent( event )
+        elements = sweetSelector.select( selector )
+        _.each( elements, function( element ) {
+          !element.dispatchEvent( our_event )
+        })
+      }
     }
-  }
-}(SweetSelector));
+  }(this.SweetSelector)),
 
-var AjaxWrapper = (function(){
-  return {
-    request: function( options ) {
-      sweetRequest = new XMLHttpRequest();
-      sweetRequest.open(options.type, options.url, true);
+  AjaxWrapper: (function(){
+    return {
+      request: function( options ) {
+        sweetRequest = new XMLHttpRequest();
+        sweetRequest.open(options.type, options.url, true);
 
-      sweetRequest.onload = function() {
-        if (sweetRequest.status >= 200 && sweetRequest.status < 400){
-          options.success();
-        }
-      };
+        sweetRequest.onload = function() {
+          if (sweetRequest.status >= 200 && sweetRequest.status < 400){
+            options.success();
+          }
+        };
 
-      sweetRequest.onerror = function() {
-        options.fail()
-      };
+        sweetRequest.onerror = function() {
+          options.fail()
+        };
 
-      sweetRequest.send();
+        sweetRequest.send();
+      }
     }
-  }
-}())
+  }())
+};
