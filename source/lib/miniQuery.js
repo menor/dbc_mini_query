06fae325699f5a7e5861247d78518fbd1e9d0359
& <pair+menor.mknudsen01@devbootcamp.com>
2014-03-18 10:14:59 -0700

    Basic SweetSelector working
    * Will select the appropriate element for classes, ids and tags
    * Does not have any iife or module pattern goodies yet

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,17 @@
 /*!
  * minQuery
  */
+
+var SweetSelector = {
+  select: function(selector){
+    var symbol = selector[0];
+    var element = selector.slice(1);
+    if(symbol == '#'){
+      return document.getElementById(element)
+    } else if (symbol == '.'){
+      return document.getElementsByClassName(element)
+    } else {
+      return document.getElementsByTagName(selector)
+    }
+  }
+}
