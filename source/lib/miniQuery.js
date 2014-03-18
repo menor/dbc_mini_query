157b19086ef02ebf0ccb2242bc3885c7588c4d51
& <pair+menor.mknudsen01@devbootcamp.com>
2014-03-18 12:07:04 -0700

    Add AJAX wrapper and change all to iife's

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -16,51 +16,74 @@ var SweetSelector = {
   }
 }
 
-var DOM = {
-  show: function(selector){
-    elements = SweetSelector.select(selector)
-    _.each(elements, function(element){
-      element.style.display="block";
-    })
-  },
+var DOM = (function(sweetSelector){
+  return {
+    show: function(selector){
+      elements = sweetSelector.select(selector)
+      _.each(elements, function(element){
+        element.style.display="block";
+      })
+    },
 
-  hide: function(selector){
-    elements = SweetSelector.select(selector)
-    _.each(elements, function(element){
-      element.style.display="none";
-    })
-  },
+    hide: function(selector){
+      elements = sweetSelector.select(selector)
+      _.each(elements, function(element){
+        element.style.display="none";
+      })
+    },
 
-  addClass: function(selector, className){
-    elements = SweetSelector.select(selector)
-    _.each(elements, function(element){
-      element.classList.add(className);
-    })
-  },
+    addClass: function(selector, className){
+      elements = sweetSelector.select(selector)
+      _.each(elements, function(element){
+        element.classList.add(className);
+      })
+    },
 
-  removeClass: function(selector, className ){
-    elements = SweetSelector.select(selector)
-    _.each(elements, function(element){
-      element.classList.remove(className);
-    })
+    removeClass: function(selector, className ){
+      elements = sweetSelector.select(selector)
+      _.each(elements, function(element){
+        element.classList.remove(className);
+      })
+    }
   }
-}
+}(SweetSelector));
 
-var EventDispatcher = {
-  on: function( selector, event, callback ) {
-    elements = SweetSelector.select( selector )
-    _.each( elements, function( element ) {
-      element.addEventListener( event, callback )
-    })
-  },
+var EventDispatcher = (function(sweetSelector){
+  return {
+    on: function( selector, event, callback ) {
+      elements = sweetSelector.select( selector )
+      _.each( elements, function( element ) {
+        element.addEventListener( event, callback )
+      })
+    },
 
-  trigger: function( selector, event ) {
-    our_event = new CustomEvent( event )
-    elements = SweetSelector.select( selector )
-    _.each( elements, function( element ) {
-      !element.dispatchEvent( our_event )
-    })
+    trigger: function( selector, event ) {
+      our_event = new CustomEvent( event )
+      elements = sweetSelector.select( selector )
+      _.each( elements, function( element ) {
+        !element.dispatchEvent( our_event )
+      })
+    }
   }
+}(SweetSelector));
 
+var AjaxWrapper = (function(){
+  return {
+    request: function( options ) {
+      sweetRequest = new XMLHttpRequest();
+      sweetRequest.open(options.type, options.url, true);
 
-}
+      sweetRequest.onload = function() {
+        if (sweetRequest.status >= 200 && sweetRequest.status < 400){
+          options.success();
+        }
+      };
+
+      sweetRequest.onerror = function() {
+        options.fail()
+      };
+
+      sweetRequest.send();
+    }
+  }
+}())
